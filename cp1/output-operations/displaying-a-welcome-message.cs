032673ca4d111ece71c4b1be0e5bdc18afdc3400
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-08 06:58:20 +0800

    Create displaying-a-welcome-message.cs

---
+++ cp1/output-operations/displaying-a-welcome-message.cs
@@ -0,0 +1,13 @@
+/* Displaying a Welcome Message
+  Write a program that greets the user by displaying the message "Hello, World!" on the console.
+  Sample Output 1
+      Hello, World! */
+
+using System;
+
+class MainClass {
+    static public void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
