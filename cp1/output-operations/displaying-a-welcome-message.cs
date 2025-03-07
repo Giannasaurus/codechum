831ab011289c221978969b971352ca71245aaa9a
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-08 07:02:07 +0800

    Update displaying-a-welcome-message.cs

--- cp1/output-operations/displaying-a-welcome-message.cs
+++ cp1/output-operations/displaying-a-welcome-message.cs
@@ -1,7 +1,7 @@
 /* Displaying a Welcome Message
   Write a program that greets the user by displaying the message "Hello, World!" on the console.
   Sample Output 1
-      Hello, World! */
+      > Hello, World! */
 
 using System;
 
