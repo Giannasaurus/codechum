c6adc10b7261edb63287aeab08ab2d265cd59907
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-08 07:29:06 +0800

    Create 01-line-break-mastery.cs

---
+++ cp1/02-basic-escape-sequences/01-line-break-mastery.cs
@@ -0,0 +1,15 @@
+/* Line Break Mastery
+  Write a program that demonstrates the usage of \n to print "Hello" and "World!" on two separate lines.
+  Sample Output 1
+    > Hello
+      World! */
+
+using System;
+
+namespace Program {
+    class program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello\nWorld!");
+        }
+    }
+}
