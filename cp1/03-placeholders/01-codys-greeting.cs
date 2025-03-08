7494ecdb82960845614234050f8cae9e73877904
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-08 17:04:23 +0800

    Create 01-codys-greeting.cs

---
+++ cp1/03-placeholders/01-codys-greeting.cs
@@ -0,0 +1,16 @@
+/* Cody's Greeting
+  Write a program that prints a greeting message of Cody with a string placeholder for its "Cody".
+  
+  Sample Output 1
+  > Hello, my name is Cody! */
+
+using System;
+
+namespace Program {
+    class main {
+        static void Main(string[] args) {
+            string name="Cody";
+            Console.WriteLine("Hello, my name is {0}!", name);
+        }
+    }
+}
