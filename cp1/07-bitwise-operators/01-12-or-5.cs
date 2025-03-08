76ee266314367b456f941f7506c148cbba15ddfa
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-08 20:37:50 +0800

    Create 01-12-or-5.cs

---
+++ cp1/07-bitwise-operators/01-12-or-5.cs
@@ -0,0 +1,13 @@
+/* 12 OR 25
+  Write a program that prints the result of 12 OR 25. Print the result only.
+
+  Sample Output 1
+  > 29 */
+
+using System;
+
+class Program {
+    static void Main() {
+        Console.Write(12|25);
+    }
+}
