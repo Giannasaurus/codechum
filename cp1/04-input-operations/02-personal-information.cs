4d1ed6a746f59caa8bfa04ce6aa00ef68fcc67b2
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-15 23:43:00 +0800

    Update 02-personal-information.cs

--- cp1/04-input-operations/02-personal-information.cs
+++ cp1/04-input-operations/02-personal-information.cs
@@ -1,33 +1,33 @@
-/* Personal Information
-  Write a program that asks the user to enter their name (string), age (int), and city (string). Print the entered values directly. The output should look like:
+/** Personal Information
+    Write a program that asks the user to enter their name (string), age (int), and city (string). Print the entered values directly. The output should look like:
 
-  Name: [Name]
-  Age: [Age] 
-  City: [City]
+    Name: [Name]
+    Age: [Age] 
+    City: [City]
 
-  Sample Output 1
-  > Enter your name: Cody
-    Enter your age: 18
-    Enter your city: Cebu
-    Name: Cody
-    Age: 18
-    City: Cebu
+    * Sample Output 1
+      > Enter your name: Cody
+        Enter your age: 18
+        Enter your city: Cebu
+        Name: Cody
+        Age: 18
+        City: Cebu
     
-  Sample Output 2
-  > Enter your name: Raul
-    Enter your age: 22
-    Enter your city: Masbate
-    Name: Raul
-    Age: 22
-    City: Masbate
+    * Sample Output 2
+      > Enter your name: Raul
+        Enter your age: 22
+          Enter your city: Masbate
+        Name: Raul
+        Age: 22
+        City: Masbate
     
-  Sample Output 3
-  > Enter your name: David
-    Enter your age: 60
-    Enter your city: Talisay
-    Name: David
-    Age: 60
-    City: Talisay */
+    * Sample Output 3
+      > Enter your name: David
+        Enter your age: 60
+        Enter your city: Talisay
+        Name: David
+        Age: 60
+        City: Talisay */
 
 using System;
 
@@ -53,3 +53,47 @@ class MainClass {
         Console.Write($"City: {city}");
     }
 }
+
+/** passed test cases
+  * test case 1:
+    > Enter your name: Cody
+      Enter your age: 18
+      Enter your city: Cebu
+      Name: Cody
+      Age: 18
+      City: Cebu
+  * test case 2:
+    > Enter your name: Raul
+      Enter your age: 22
+      Enter your city: Masbate
+      Name: Raul
+      Age: 22
+      City: Masbate
+  * test case 3:
+    > Enter your name: David
+      Enter your age: 60
+      Enter your city: Talisay
+      Name: David
+      Age: 60
+      City: Talisay
+  * test case 4:
+    > Enter your name: Mark
+      Enter your age: 30
+      Enter your city: Naga
+      Name: Mark
+      Age: 30
+      City: Naga
+  * test case 5:
+    > Enter your name: Johnray
+      Enter your age: 5
+      Enter your city: Minglanilla
+      Name: Johnray
+      Age: 5
+      City: Minglanilla
+  * test case 6:
+    > Enter your name: Ian
+      Enter your age: 33
+      Enter your city: Manila
+      Name: Ian
+      Age: 33
+      City: Manila */
