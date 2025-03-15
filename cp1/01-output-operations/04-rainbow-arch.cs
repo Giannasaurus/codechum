1ed99c16e27cb4d0565bfc9d656e4d292083066b
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-15 23:22:10 +0800

    Update 04-rainbow-arch.cs

--- cp1/01-output-operations/04-rainbow-arch.cs
+++ cp1/01-output-operations/04-rainbow-arch.cs
@@ -1,17 +1,17 @@
-/* Rainbow Arch
-  Write a program that brings the vibrant beauty of a rainbow to life by gracefully printing a colorful arch of hues. Copy the number of spaces before each color to mimic the arch of the rainbow as depicted below:
+/** Rainbow Arch
+    Write a program that brings the vibrant beauty of a rainbow to life by gracefully printing a colorful arch of hues. Copy the number of spaces before each color to mimic the arch of the rainbow as depicted below:
 
-              Green
-        Yellow     Brown
-    Orange             Indigo
-  Red                      Violet
+                Green
+          Yellow     Brown
+      Orange             Indigo
+    Red                      Violet
 
-  Sample Output 1
-  >
-              Green
-        Yellow     Brown
-    Orange             Indigo
-  Red                      Violet */
+    * Sample Output 1
+      >
+                Green
+          Yellow     Brown
+      Orange             Indigo
+    Red                      Violet */
 
 using System;
 
@@ -23,3 +23,10 @@ class MainClass {
         Console.WriteLine("Red" + "                      Violet");
     }
 }
+
+/** passed test cases
+  * test case 1:
+    >           Green
+          Yellow     Brown
+      Orange             Indigo
+    Red                      Violet */
