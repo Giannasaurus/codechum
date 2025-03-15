8b80f793abc6f27997b8f6a0dccb72c55f7215ff
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-15 23:38:52 +0800

    Update 01-name-input.cs

--- cp1/04-input-operations/01-name-input.cs
+++ cp1/04-input-operations/01-name-input.cs
@@ -1,17 +1,17 @@
-/* Name Input
-  Write a program that prompts the user to enter their name and then prints a welcome message that includes their name.
+/** Name Input
+    Write a program that prompts the user to enter their name and then prints a welcome message that includes their name.
 
-  Sample Output 1
-  > Enter your name: Cody
-    Welcome, Cody!
+    * Sample Output 1
+      > Enter your name: Cody
+        Welcome, Cody!
 
-  Sample Output 2
-  > Enter your name: David
-    Welcome, David!
+    * Sample Output 2
+      > Enter your name: David
+        Welcome, David!
 
-  Sample Output 3
-  > Enter your name: Vincent
-    Welcome, Vincent! */
+    * Sample Output 3
+      > Enter your name: Vincent
+        Welcome, Vincent! */
 
 using System;
 
@@ -32,8 +32,20 @@ class MainClass {
 
 /** passed test cases
   * test case 1:
-  * test case 1:
-  * test case 1:
-  * test case 1:
-  * test case 1:
-  * test case 1:
+    > Enter your name: Cody
+      Welcome, Cody!
+  * test case 2:
+    > Enter your name: David
+      Welcome, David!
+  * test case 3:
+    > Enter your name: Vincent
+      Welcome, Vincent!
+  * test case 4:
+    > Enter your name: Raul
+      Welcome, Raul!
+  * test case 5:
+    > Enter your name: Johnray
+      Welcome, Johnray!
+  * test case 6: 
+    > Enter your name: Elon
+      Welcome, Elon! */
