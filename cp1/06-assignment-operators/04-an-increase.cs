fd615c5edf598a8f3445befd9e7455583d9ff164
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-16 15:45:53 +0800

    Update 04-an-increase.cs

--- cp1/06-assignment-operators/04-an-increase.cs
+++ cp1/06-assignment-operators/04-an-increase.cs
@@ -1,17 +1,17 @@
-/* An Increase
-  Ask for a float number price then multiply the value of price by 1.1 to add a 10% increase using the *= operator. Print the final value in two decimal places.
+/** An Increase
+    Ask for a float number price then multiply the value of price by 1.1 to add a 10% increase using the *= operator. Print the final value in two decimal places.
 
-  Sample Output 1
-  > Enter the price: 10
-    The new price is: 11.00
+    * Sample Output 1
+      > Enter the price: 10
+        The new price is: 11.00
 
-  Sample Output 2
-  > Enter the price: 100
-    The new price is: 110.00
+    * Sample Output 2
+      > Enter the price: 100
+        The new price is: 110.00
 
-  Sample Output 3
-  > Enter the price: -5
-    The new price is: -5.50 */
+    * Sample Output 3
+      > Enter the price: -5
+        The new price is: -5.50 */
 
 using System;
 
@@ -31,3 +31,23 @@ class Program {
         Console.Write("The new price is: {0:f2}", price);
     }
 }
+
+/** passed test cases
+  * test case 1:
+    > Enter the price: 10
+      The new price is: 11.00
+  * test case 2:
+    > Enter the price: 100
+      The new price is: 110.00
+  * test case 3:
+    > Enter the price: -5
+      The new price is: -5.50
+  * test case 4:
+    > Enter the price: 647.41
+      The new price is: 712.15
+  * test case 5:
+    > Enter the price: 1087.46
+      The new price is: 1196.21
+  * test case 6:
+    > Enter the price: 77
+      The new price is: 84.70 */
