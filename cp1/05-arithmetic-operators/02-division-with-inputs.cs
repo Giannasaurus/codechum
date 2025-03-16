ef5e6d3b7022a02426a1fcb22c1a2c8e03c3dc03
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-16 15:24:25 +0800

    Update 02-division-with-inputs.cs

--- cp1/05-arithmetic-operators/02-division-with-inputs.cs
+++ cp1/05-arithmetic-operators/02-division-with-inputs.cs
@@ -1,20 +1,20 @@
-/* Division with Inputs
-  Write a program that takes two numbers, x, and y, as input from the user and prints the quotient when x is divided by y.
+/** Division with Inputs
+    Write a program that takes two numbers, x, and y, as input from the user and prints the quotient when x is divided by y.
   
-  Sample Output 1
-  > Enter x: 10
-    Enter y: 2
-    Quotient: 5
+    * Sample Output 1
+      > Enter x: 10
+        Enter y: 2
+        Quotient: 5
     
-  Sample Output 2
-  > Enter x: 6
-    Enter y: 3
-    Quotient: 2
+    * Sample Output 2
+      > Enter x: 6
+        Enter y: 3
+        Quotient: 2
     
-  Sample Output 3
-  > Enter x: 4
-    Enter y: 2
-    Quotient: 2 */
+    * Sample Output 3
+    > Enter x: 4
+      Enter y: 2
+      Quotient: 2 */
 
 using System;
 
@@ -37,3 +37,29 @@ class MainClass {
         Console.Write("Quotient: " + quotient);
     }
 }
+
+/** passed test cases
+  * test case 1:
+    > Enter x: 10
+      Enter y: 2
+      Quotient: 5
+  * test case 2:
+    > Enter x: 6
+      Enter y: 3
+      Quotient: 2
+  * test case 3:
+    > Enter x: 4
+      Enter y: 2
+      Quotient: 2
+  * test case 4:
+    > Enter x: -200
+      Enter y: 5
+      Quotient: -40
+  * test case 5:
+    > Enter x: 20
+      Enter y: 4
+      Quotient: 5
+  * test case 6:
+    > Enter x: 1221
+      Enter y: 3
+      Quotient: 407 */
