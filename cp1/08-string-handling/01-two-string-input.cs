4ead1e7b14371f2877ca7aa8325f4d48a6667f95
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-16 15:58:51 +0800

    Update 01-two-string-input.cs

--- cp1/08-string-handling/01-two-string-input.cs
+++ cp1/08-string-handling/01-two-string-input.cs
@@ -1,20 +1,20 @@
-/* Two String Input
-  Write a program that asks for two string inputs, first and second, and prints the strings in the following format: "{first} {second} {first}"
+/** Two String Input
+    Write a program that asks for two string inputs, first and second, and prints the strings in the following format: "{first} {second} {first}"
   
-  > Sample Output 1
-    Enter the first string: Hello
-    Enter the second string: World
-    Hello World Hello
+  * Sample Output 1
+    > Enter the first string: Hello
+      Enter the second string: World
+      Hello World Hello
 
-  > Sample Output 2
-    Enter the first string: Code
-    Enter the second string: Chum
-    Code Chum Code
+  * Sample Output 2
+    > Enter the first string: Code
+      Enter the second string: Chum
+      Code Chum Code
 
-  > Sample Output 3
-    Enter the first string: First
-    Enter the second string: Second
-    First Second First */
+  * Sample Output 3
+    > Enter the first string: First
+      Enter the second string: Second
+      First Second First */
 
 using System;
 
@@ -36,26 +36,26 @@ class Program {
 
 /** passed test cases
   * test case 1:
-    Enter the first string: Hello
-    Enter the second string: World
-    Hello World Hello
+    > Enter the first string: Hello
+      Enter the second string: World
+      Hello World Hello
   * test case 2:
-    Enter the first string: Code
-    Enter the second string: Chum
-    Code Chum Code
+    > Enter the first string: Code
+      Enter the second string: Chum
+      Code Chum Code
   * test case 3:
-    Enter the first string: First
-    Enter the second string: Second
-    First Second First
+    > Enter the first string: First
+      Enter the second string: Second
+      First Second First
   * test case 4:
-    Enter the first string: One
-    Enter the second string: Piece
-    One Piece One
+    > Enter the first string: One
+      Enter the second string: Piece
+      One Piece One
   * test case 5:
-    Enter the first string: Python
-    Enter the second string: Java
-    Python Java Python
+    > Enter the first string: Python
+      Enter the second string: Java
+      Python Java Python
   * test case 6:
-    Enter the first string: Chum
-    Enter the second string: Code
-    Chum Code Chum */
+    > Enter the first string: Chum
+      Enter the second string: Code
+      Chum Code Chum */
