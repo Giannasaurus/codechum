b00c0149a97fcd37544dcc666c998f774132b8a4
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-16 16:04:04 +0800

    Update 01-string-to-integer.cs

--- cp1/09-typecasting/01-string-to-integer.cs
+++ cp1/09-typecasting/01-string-to-integer.cs
@@ -1,17 +1,17 @@
-/* String to Integer
-  Write a program that takes a string input and then converts the input into an integer. Print the new integer in format "String converted to integer: {integer}".
+/** String to Integer
+    Write a program that takes a string input and then converts the input into an integer. Print the new integer in format "String converted to integer: {integer}".
 
   * Sample Output 1
-    Enter a string: 222
-    String converted to integer: 222
+    > Enter a string: 222
+      String converted to integer: 222
 
   * Sample Output 2
-    Enter a string: 343
-    String converted to integer: 343
+    > Enter a string: 343
+      String converted to integer: 343
 
   * Sample Output 3
-    Enter a string: 243
-    String converted to integer: 243 */
+    > Enter a string: 243
+      String converted to integer: 243 */
 
 using System;
 
@@ -35,20 +35,20 @@ class Program {
 
 /** passed test cases
   * test case 1:
-    Enter a string: 222
-    String converted to integer: 222
+    > Enter a string: 222
+      String converted to integer: 222
   * test case 2:
-    Enter a string: 343
-    String converted to integer: 343
+    > Enter a string: 343
+      String converted to integer: 343
   * test case 3:
-    Enter a string: 243
-    String converted to integer: 243
+    > Enter a string: 243
+      String converted to integer: 243
   * test case 4:
-    Enter a string: 2511
-    String converted to integer: 2511
+    > Enter a string: 2511
+      String converted to integer: 2511
   * test case 5:
-    Enter a string: 3312
-    String converted to integer: 3312
+    > Enter a string: 3312
+      String converted to integer: 3312
   * test case 6:
-    Enter a string: 512
-    String converted to integer: 512 */
+    > Enter a string: 512
+      String converted to integer: 512 */
