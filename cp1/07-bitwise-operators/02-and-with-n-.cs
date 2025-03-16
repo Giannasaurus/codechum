686f787feb4814f1826d4ef8952586dc4f3acc22
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-16 15:51:34 +0800

    Update 02-and-with-n-.cs

--- cp1/07-bitwise-operators/02-and-with-n-.cs
+++ cp1/07-bitwise-operators/02-and-with-n-.cs
@@ -1,17 +1,17 @@
-/* AND with N
-  Write a program that asks for an integer N, and then do the AND operator with N and 25. Print the result in the format: N AND 25 = ?
+/** AND with N
+    Write a program that asks for an integer N, and then do the AND operator with N and 25. Print the result in the format: N AND 25 = ?
 
-  Sample Output 1
-  > Enter an integer: 17
-    17 AND 25 = 17
+    * Sample Output 1
+      > Enter an integer: 17
+        17 AND 25 = 17
 
-  Sample Output 2
-  > Enter an integer: 101
-  101 AND 25 = 1
+    * Sample Output 2
+      > Enter an integer: 101
+        101 AND 25 = 1
 
-  Sample Output 3
-  > Enter an integer: 79
-    79 AND 25 = 9 */
+    * Sample Output 3
+      > Enter an integer: 79
+        79 AND 25 = 9 */
 
 using System;
 
@@ -27,3 +27,23 @@ class Program {
         Console.Write($"{N} AND 25 = {result}");
     }
 }
+
+/** passed test cases
+  * test case 1:
+    > Enter an integer: 17
+      17 AND 25 = 17
+  * test case 2:
+    > Enter an integer: 101
+      101 AND 25 = 1
+  * test case 3:
+    > Enter an integer: 79
+      79 AND 25 = 9
+  * test case 4:
+    > Enter an integer: 446
+      446 AND 25 = 24
+  * test case 5:
+    > Enter an integer: 55
+      55 AND 25 = 17
+  * test case 6:
+    > Enter an integer: 78
+    78 AND 25 = 8 */
