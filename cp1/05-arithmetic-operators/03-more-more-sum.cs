a56bcea46fb4df8c9ec2f9a88c598fc231aab04e
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-16 15:26:30 +0800

    Update 03-more-more-sum.cs

--- cp1/05-arithmetic-operators/03-more-more-sum.cs
+++ cp1/05-arithmetic-operators/03-more-more-sum.cs
@@ -1,23 +1,23 @@
-/* More, more sum
-  Write a program that takes in three integer inputs, x, y, and z, and prints their sum.
+/** More, more sum
+    Write a program that takes in three integer inputs, x, y, and z, and prints their sum.
   
-  Sample Output 1
-  > Enter x: 10
-    Enter y: 24
-    Enter z: 3
-    Sum: 37
+    * Sample Output 1
+      > Enter x: 10
+        Enter y: 24
+        Enter z: 3
+        Sum: 37
     
-  Sample Output 2
-  > Enter x: 7
-    Enter y: 9
-    Enter z: 4
-    Sum: 20
+    * Sample Output 2
+      > Enter x: 7
+        Enter y: 9
+        Enter z: 4
+        Sum: 20
     
-  Sample Output 3
-  > Enter x: 20
-    Enter y: 46
-    Enter z: 56
-    Sum: 122 */
+    * Sample Output 3
+      > Enter x: 20
+        Enter y: 46
+        Enter z: 56
+        Sum: 122 */
 
 using System;
 
@@ -42,3 +42,35 @@ class MainClass {
         Console.Write("Sum: " + sum);
     }
 }
+
+/** passed test cases
+  * test case 1:
+    > Enter x: 10
+      Enter y: 24
+      Enter z: 3
+      Sum: 37
+  * test case 2:
+    > Enter x: 7
+      Enter y: 9
+      Enter z: 4
+      Sum: 20
+  * test case 3:
+    > Enter x: 20
+      Enter y: 46
+      Enter z: 56
+      Sum: 122
+  * test case 4:
+    > Enter x: 20
+      Enter y: 84
+      Enter z: 66
+      Sum: 170
+  * test case 5:
+    > Enter x: 46
+      Enter y: 34
+      Enter z: 22
+      Sum: 102
+  * test case 6:
+    > Enter x: 100
+      Enter y: 99
+      Enter z: 747
+      Sum: 946 */
