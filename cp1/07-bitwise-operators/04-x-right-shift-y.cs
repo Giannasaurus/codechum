925901ce71b1bdcca642ac3bcfedd056e237bda8
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-16 15:54:51 +0800

    Update 04-x-right-shift-y.cs

--- cp1/07-bitwise-operators/04-x-right-shift-y.cs
+++ cp1/07-bitwise-operators/04-x-right-shift-y.cs
@@ -1,20 +1,20 @@
-/* X Right Shift Y
-  Write a program that asks for two integers X and Y, and then right-shift X by Y. Print the result in the format: X >> Y = ?
+/** X Right Shift Y
+    Write a program that asks for two integers X and Y, and then right-shift X by Y. Print the result in the format: X >> Y = ?
 
-  Sample Output 1
-  > Enter X: 25
-    Enter Y: 46
-    25 >> 46 = 0
+    * Sample Output 1
+      > Enter X: 25
+        Enter Y: 46
+        25 >> 46 = 0
 
-  Sample Output 2
-  > Enter X: 25
-    Enter Y: 1
-    25 >> 1 = 12
+    * Sample Output 2
+      > Enter X: 25
+        Enter Y: 1
+        25 >> 1 = 12
 
-  Sample Output 3
-  > Enter X: 1024
-    Enter Y: 3
-    1024 >> 3 = 128 */
+    * Sample Output 3
+      > Enter X: 1024
+        Enter Y: 3
+        1024 >> 3 = 128 */
 
 using System;
 
@@ -32,3 +32,29 @@ class Program {
         Console.Write($"{X} >> {Y} = {result}");
     }
 }
+
+/** passed test cases
+  * test case 1:
+    > Enter X: 25
+      Enter Y: 46
+      25 >> 46 = 0
+  * test case 2:
+    > Enter X: 25
+      Enter Y: 1
+      25 >> 1 = 12
+  * test case 3:
+    > Enter X: 1024
+      Enter Y: 3
+      1024 >> 3 = 128
+  * test case 4:
+    > Enter X: 4
+      Enter Y: 2
+      4 >> 2 = 1
+  * test case 5:
+    > Enter X: 23
+      Enter Y: 51
+      23 >> 51 = 0
+  * test case 6:
+    > Enter X: 47
+      Enter Y: 7
+      47 >> 7 = 0 */
