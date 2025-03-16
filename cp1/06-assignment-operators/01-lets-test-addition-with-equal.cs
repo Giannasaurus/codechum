ea8cb050294d04b535835960e32c9330d1425a1e
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-16 15:36:12 +0800

    Update 01-lets-test-addition-with-equal.cs

--- cp1/06-assignment-operators/01-lets-test-addition-with-equal.cs
+++ cp1/06-assignment-operators/01-lets-test-addition-with-equal.cs
@@ -1,17 +1,17 @@
-/* Let's Test Addition with Equal
-  Write a program that asks for an integer number, adds 1, and assigns to the number using the += operator.
+/** Let's Test Addition with Equal
+    Write a program that asks for an integer number, adds 1, and assigns to the number using the += operator.
   
-  Sample Output 1
-  > Enter an integer number: 1
-    The updated number is: 2
+    * Sample Output 1
+      > Enter an integer number: 1
+        The updated number is: 2
     
-  Sample Output 2
-  > Enter an integer number: 2
-    The updated number is: 3
+    * Sample Output 2
+      > Enter an integer number: 2
+       The updated number is: 3
     
-  Sample Output 3
-  > Enter an integer number: 3
-    The updated number is: 4 */
+    * Sample Output 3
+      > Enter an integer number: 3
+        The updated number is: 4 */
 
 using System;
 
@@ -31,3 +31,23 @@ class Program {
         Console.Write("The updated number is: {0}", num);
     }
 }
+
+/** passed test cases
+  * test case 1:
+    > Enter an integer number: 1
+      The updated number is: 2
+  * test case 2:
+    > Enter an integer number: 2
+      The updated number is: 3
+  * test case 3:
+    > Enter an integer number: 3
+      The updated number is: 4
+  * test case 4:
+    > Enter an integer number: 4
+      The updated number is: 5
+  * test case 5:
+    > Enter an integer number: 5
+      The updated number is: 6
+  * test case 6:
+    > Enter an integer number: -1
+      The updated number is: 0 */
