252da7c80ab54a28cf3078d639765c60c840e8ca
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-16 16:00:50 +0800

    Update 03-two-lovers.cs

--- cp1/08-string-handling/03-two-lovers.cs
+++ cp1/08-string-handling/03-two-lovers.cs
@@ -1,20 +1,20 @@
-/* Two Lovers
-  Write a program that asks for two string inputs, lover1, and lover2. After asking for input, print in the format: "{lover1} and {lover2} are in love."
+/** Two Lovers
+    Write a program that asks for two string inputs, lover1, and lover2. After asking for input, print in the format: "{lover1} and {lover2} are in love."
   
-  > Sample Output 1
-    Enter lover1: Jane
-    Enter lover2: John
-    Jane and John are in love.
+  * Sample Output 1
+    > Enter lover1: Jane
+      Enter lover2: John
+      Jane and John are in love.
 
-  > Sample Output 2
-    Enter lover1: Pela
-    Enter lover2: Lynx
-    Pela and Lynx are in love.
+  * Sample Output 2
+    > Enter lover1: Pela
+      Enter lover2: Lynx
+      Pela and Lynx are in love.
 
-  > Sample Output 3
-    Enter lover1: John
-    Enter lover2: Doe
-    John and Doe are in love. */
+  * Sample Output 3
+    > Enter lover1: John
+      Enter lover2: Doe
+      John and Doe are in love. */
 
 using System;
 
@@ -36,26 +36,26 @@ class Program {
 
 /** passed test cases
   * test case 1:
-    Enter lover1: Jane
-    Enter lover2: John
-    Jane and John are in love.
+    > Enter lover1: Jane
+      Enter lover2: John
+      Jane and John are in love.
   * test case 2:
-    Enter lover1: Pela
-    Enter lover2: Lynx
-    Pela and Lynx are in love.
+    > Enter lover1: Pela
+      Enter lover2: Lynx
+      Pela and Lynx are in love.
   * test case 3:
-    Enter lover1: John
-    Enter lover2: Doe
-    John and Doe are in love.
+    > Enter lover1: John
+      Enter lover2: Doe
+      John and Doe are in love.
   * test case 4:
-    Enter lover1: Cody
-    Enter lover2: Dog
-    Cody and Dog are in love.
+    > Enter lover1: Cody
+      Enter lover2: Dog
+      Cody and Dog are in love.
   * test case 5:
-    Enter lover1: Jynx
-    Enter lover2: Rest
-    Jynx and Rest are in love.
+    > Enter lover1: Jynx
+      Enter lover2: Rest
+      Jynx and Rest are in love.
   * test case 6:
-    Enter lover1: Loucha
-    Enter lover2: Kallen
-    Loucha and Kallen are in love. */
+    > Enter lover1: Loucha
+      Enter lover2: Kallen
+      Loucha and Kallen are in love. */
