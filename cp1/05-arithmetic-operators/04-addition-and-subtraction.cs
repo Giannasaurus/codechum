125947e03bf90b350b600b2b0721667fbf527833
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-03-16 15:28:20 +0800

    Update 04-addition-and-subtraction.cs

--- cp1/05-arithmetic-operators/04-addition-and-subtraction.cs
+++ cp1/05-arithmetic-operators/04-addition-and-subtraction.cs
@@ -1,23 +1,23 @@
-/* Addition and Subtraction
-  Write a program that accepts two integer inputs, x, and y. Show the output when x is added by y, and also, when x is subtracted by y.
+/** Addition and Subtraction
+    Write a program that accepts two integer inputs, x, and y. Show the output when x is added by y, and also, when x is subtracted by y.
   
-  Sample Output 1
-  > Enter x: 10
-    Enter y: 5
-    10 + 5 = 15
-    10 - 5 = 5
+    * Sample Output 1
+      > Enter x: 10
+        Enter y: 5
+        10 + 5 = 15
+        10 - 5 = 5
     
-  Sample Output 2
-  > Enter x: 18
-    Enter y: 7
-    18 + 7 = 25
-    18 - 7 = 11
+    * Sample Output 2
+      > Enter x: 18
+        Enter y: 7
+        18 + 7 = 25
+        18 - 7 = 11
     
-  Sample Output 3
-  > Enter x: 37
-    Enter y: 66
-    37 + 66 = 103
-    37 - 66 = -29 */
+    * Sample Output 3
+      > Enter x: 37
+        Enter y: 66
+        37 + 66 = 103
+        37 - 66 = -29 */
 
 using System;
 
@@ -42,3 +42,35 @@ class MainClass {
         Console.WriteLine($"{x} - {y} = {diff}");
     }
 }
+
+/** passed test cases
+  * test case 1:
+    > Enter x: 10
+      Enter y: 5
+      10 + 5 = 15
+      10 - 5 = 5
+  * test case 2:
+    > Enter x: 18
+      Enter y: 7
+      18 + 7 = 25
+      18 - 7 = 11
+  * test case 3:
+    > Enter x: 37
+      Enter y: 66
+      37 + 66 = 103
+      37 - 66 = -29
+  * test case 4:
+    > Enter x: 84
+      Enter y: 7
+      84 + 7 = 91
+      84 - 7 = 77
+  * test case 5:
+    > Enter x: 0
+      Enter y: 0
+      0 + 0 = 0
+      0 - 0 = 0
+  * test case 6:
+    > Enter x: 1
+      Enter y: 1
+      1 + 1 = 2
+      1 - 1 = 0*/
